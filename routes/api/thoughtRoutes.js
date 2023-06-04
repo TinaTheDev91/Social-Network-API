332b28687f52d17c4bfeaa5f5bd8e3b34388e394
TinaTheDev91 <122223572+TinaTheDev91@users.noreply.github.com>
2023-06-04 19:26:57 -0400

    finished routes, pending testing

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThought)
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+router
+    .route('/:thoughtId/reactions')
+    .put(addReaction)
+    .delete(removeReaction)
